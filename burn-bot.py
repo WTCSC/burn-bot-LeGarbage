cd212c94ced3d5d72e0c01a8bb2b8ed160885fb5
Logan Lessen <loganlessen@gmail.com>
2025-08-26 18:23:52 -0600

    Added questions and basic output

---
+++ burn-bot.py
@@ -0,0 +1,11 @@
+name = input("What's your name? ")
+age = input("How old are you? ")
+activity = input("What's something you like to do? ")
+wake_up_time = input("What time did you wake up this morning? ")
+breakfast = input("What did you have for breakfast? ")
+
+print(f"Your name is {name}\n" +
+      f"You are {age} years old\n" +
+      f"You like to {activity}\n" +
+      f"You woke up at {wake_up_time} this morning\n" +
+      f"You ate {breakfast} for breakfast")
